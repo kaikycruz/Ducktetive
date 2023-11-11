a9b7c00df55913eb34ab27627136c0ba3964e3a6
MarcceloSpinelli <marcelo.spinelli@sptech.school>
2023-11-11 19:36:39 -0300

    teste9

--- JAR INDIVIDUAL/MARCCELO SPINELLI/src/main/java/Conexao.java
+++ JAR INDIVIDUAL/MARCCELO SPINELLI/src/main/java/Conexao.java
@@ -7,7 +7,7 @@ public class Conexao {
     public Conexao() {
         BasicDataSource dataSourceServer = new BasicDataSource();
         dataSourceServer.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceServer.setUrl("jdbc:mysql://localhost:3306/Ducktetive");
+        dataSourceServer.setUrl("jdbc:mysql://127.0.0.1:3306/Ducktetive");
         dataSourceServer.setUsername("root");
         dataSourceServer.setPassword("urubu100");
 
