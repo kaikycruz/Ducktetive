49952e277aa734b8164d54beac219c26c5115840
MarcceloSpinelli <marcelo.spinelli@sptech.school>
2023-11-14 19:23:16 -0300

    teste10

--- JAR INDIVIDUAL/MARCCELO SPINELLI/src/main/java/Conexao.java
+++ JAR INDIVIDUAL/MARCCELO SPINELLI/src/main/java/Conexao.java
@@ -1,15 +1,14 @@
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 public class Conexao {
     private JdbcTemplate conexaoBanco;
 
     public Conexao(){
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/Ducktetive");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/Ducktetive100?autoReconnect=true&useSSL=false");
         dataSource.setUsername("root");
-        dataSource.setPassword("sptech");
+        dataSource.setPassword("urubu100");
 
         conexaoBanco = new JdbcTemplate(dataSource);
     }
