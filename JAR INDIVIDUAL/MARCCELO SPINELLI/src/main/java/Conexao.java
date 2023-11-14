97046b5416be9517926f1278410595e76debc554
MarcceloSpinelli <marcelo.spinelli@sptech.school>
2023-11-14 19:40:08 -0300

    testeTeste

--- JAR INDIVIDUAL/MARCCELO SPINELLI/src/main/java/Conexao.java
+++ JAR INDIVIDUAL/MARCCELO SPINELLI/src/main/java/Conexao.java
@@ -3,21 +3,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class Conexao {
     private JdbcTemplate conexaoBanco;
 
-<<<<<<< HEAD
-    public Conexao(){
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/Ducktetive100?autoReconnect=true&useSSL=false");
-        dataSource.setUsername("root");
-        dataSource.setPassword("urubu100");
-=======
+
     public Conexao() {
         BasicDataSource dataSourceServer = new BasicDataSource();
         dataSourceServer.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceServer.setUrl("jdbc:mysql://localhost:3306/Ducktetive?autoReconnect=true&useSSL=false");
-        dataSourceServer.setUsername("root");
+        dataSourceServer.setUrl("jdbc:mysql://localhost:3306/Ducktetive100?autoReconnect=true&useSSL=false");
+        dataSourceServer.setUsername("teste");
         dataSourceServer.setPassword("urubu100");
->>>>>>> refs/remotes/origin/main
+
 
         conexaoBanco = new JdbcTemplate(dataSourceServer);
     }
