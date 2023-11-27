32f5416fc21b3e6743f8c7a6f7fdcc5de5988830
JRamonP <joseramonpereira@hotmail.com>
2023-11-27 17:57:57 -0300

    Conexão Sql Server Funcionando

--- api-ducktetive/src/main/java/Banco/ConexaoBanco.java
+++ api-ducktetive/src/main/java/Banco/ConexaoBanco.java
@@ -5,35 +5,53 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ConexaoBanco {
     private JdbcTemplate conexaoBanco;
+    // ALTERAR DEV PARA INSERIR NO BANCO
+    // dev = true; <- INSERINDO LOCAL
+    // dev = false; < - INSERINDO SQL Server
+    private Boolean dev = true;
 
-    private JdbcTemplate conexaoBancoAWS;
 
-    public ConexaoBanco(){
-        // CONEXÃO LOCAL
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/Ducktetive");
-        dataSource.setUsername("root");
-        dataSource.setPassword("997849791abc");
+    public ConexaoBanco() {
 
-        conexaoBanco = new JdbcTemplate(dataSource);
+        if (dev) {
+            // CONEXÃO LOCAL
+            BasicDataSource dataSource = new BasicDataSource();
+            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/Ducktetive");
+            dataSource.setUsername("root");
+            dataSource.setPassword("997849791abc");
 
-        // CONEXÃO AZURE
-        BasicDataSource dataSourceAWS = new BasicDataSource();
-        dataSourceAWS.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSourceAWS.setUrl("jdbc:sqlserver://44.203.84.7:1433;databaseName=Ducktetive;encrypt=true;trustServerCertificate=false;");
-        dataSourceAWS.setUsername("sa");
-        dataSourceAWS.setPassword("urubu100");
+            conexaoBanco = new JdbcTemplate(dataSource);
+        } else {
+            BasicDataSource dataSourceAWS = new BasicDataSource();
+            dataSourceAWS.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            dataSourceAWS.setUrl("jdbc:sqlserver://54.174.254.124;" +
+                    "database=Ducktetive;" +
+                    "user=sa;" +
+                    "password=urubu100;" +
+                    "trustServerCertificate=true;");
+            dataSourceAWS.setUsername("sa");
+            dataSourceAWS.setPassword("urubu100");
 
-        conexaoBancoAWS = new JdbcTemplate(dataSourceAWS);
-
+            conexaoBanco = new JdbcTemplate(dataSourceAWS);
+        }
     }
 
-    public JdbcTemplate getConexaoBancoAWS() {
-        return conexaoBancoAWS;
-    }
-
-    public JdbcTemplate getConexaoBanco(){
+    public JdbcTemplate getConexaoBanco() {
         return conexaoBanco;
     }
+
+    public void setConexaoBanco(JdbcTemplate conexaoBanco) {
+        this.conexaoBanco = conexaoBanco;
+    }
+
+    public Boolean getDev() {
+        return dev;
+    }
+
+    public void setDev(Boolean dev) {
+        this.dev = dev;
+    }
+
 }
+
